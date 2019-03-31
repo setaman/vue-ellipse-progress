a7b617d0581b491ea33788dd7e82019b3de7d580
Sergej Atamantschuk <sergatam@yandex.com>
2019-03-31 21:54:36 +0200

    chore: add feat branch for new features

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <ellipse-progress-container :progress="parseFloat(progress)" :size="300" :thickness="20"
+    <ellipse-progress-container :progress="parseFloat(progress)"
+                                :size="300"
+                                :thickness="20"
                                 :empty_thickness="10"
                                 font_size="5rem"/>
     <label for="progress">
