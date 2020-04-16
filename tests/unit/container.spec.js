8d2af12b2c111a0391838fd2f2d47abc3a87099d
Sergej Atamantschuk <sergatam@yandex.com>
2020-04-16 20:54:18 +0200

    test: refactor container tests

--- tests/unit/container.spec.js
+++ tests/unit/container.spec.js
@@ -1,54 +1,35 @@
 import { expect } from "chai";
-import { shallowMount, mount } from "@vue/test-utils";
-import Container from "../../src/components/VueEllipseProgress.vue";
+import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import VueEllipseProgress from "../../src/components/VueEllipseProgress.vue";
+
+const factory = propsData => {
+  return shallowMount(VueEllipseProgress, {
+    propsData: { ...propsData }
+  });
+};
 
 describe("[ EllipseProgressContainer.vue ]", () => {
-  /* describe("rendering", () => {
-    it("renders the progress and empty circles", () => {
-      const wrapper = mount(Container, {
-        propsData: { progress: 50 }
-      });
-
-      expect(wrapper.find("circle.ep-circle--empty").exists()).to.equal(true);
-      expect(wrapper.find("circle.ep-circle--progress").exists()).to.equal(true);
-    });
-  }); */
   describe("#size", () => {
+    const size = 250;
+    const wrapper = factory({ size, progress: 50 });
     it("sets the size of the container correctly", () => {
-      const size = 250;
-      const wrapper = shallowMount(Container, {
-        propsData: { size, progress: 50 }
-      });
-      expect(wrapper.element.style.maxWidth).to.equal("250px");
-      expect(wrapper.element.style.maxHeight).to.equal("250px");
+      expect(wrapper.element.style.maxWidth).to.equal(`${size}px`);
+      expect(wrapper.element.style.maxHeight).to.equal(`${size}px`);
     });
 
     it("sets the size of the svg container correctly", () => {
-      const size = 250;
-      const wrapper = shallowMount(Container, {
-        propsData: { size, progress: 50 }
-      });
-
       const svgWrapper = wrapper.find("svg");
 
-      expect(svgWrapper.element.getAttribute("width")).to.equal("250");
-      expect(svgWrapper.element.getAttribute("height")).to.equal("250");
-    });
-  });
-  describe("#angle", () => {
-    it("sets the rotation of the svg container correctly", () => {
-      // TODO: move to circle
-    });
-    it("sets the rotation of the svg container to default, if not defined", () => {
-      // TODO: move to circle
+      expect(svgWrapper.element.getAttribute("width")).to.equal(`${size}`);
+      expect(svgWrapper.element.getAttribute("height")).to.equal(`${size}`);
     });
   });
   describe("#progress", () => {
+    let progress = 40;
+    const wrapper = factory({ progress });
+
     it("counts the decimals correctly", () => {
-      let progress = 40;
-      const wrapper = shallowMount(Container, {
-        propsData: { progress }
-      });
       expect(wrapper.vm.countDecimals).to.equal(0);
 
       progress = 56.34;
@@ -59,11 +40,10 @@ describe("[ EllipseProgressContainer.vue ]", () => {
       wrapper.setProps({ progress });
       expect(wrapper.vm.countDecimals).to.equal(4);
     });
-    it("forces noData state, if invalid", () => {
-      const progress = "notANumber";
-      const wrapper = shallowMount(Container, {
-        propsData: { progress }
-      });
+    it("forces noData state, if invalid", async () => {
+      wrapper.setProps({ progress: "notNumber" });
+      await Vue.nextTick();
+
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.classes()).to.include("ep-hidden");
       expect(wrapper.vm.isDataAvailable).to.equal(false);
@@ -71,10 +51,9 @@ describe("[ EllipseProgressContainer.vue ]", () => {
   });
   describe("#legendValue", () => {
     const progress = 40;
+    const wrapper = factory({ progress });
+
     it("counts the decimals correctly", () => {
-      const wrapper = shallowMount(Container, {
-        propsData: { progress }
-      });
       expect(wrapper.vm.countDecimals).to.equal(0);
 
       let legendValue = 124.34;
@@ -87,18 +66,13 @@ describe("[ EllipseProgressContainer.vue ]", () => {
     });
     it("replaces the progress by legendValue as the legend of the circle", () => {
       const legendValue = 324;
-      const wrapper = shallowMount(Container, {
-        propsData: { progress, legendValue }
-      });
+      wrapper.setProps({ legendValue });
       expect(wrapper.vm.legendVal).to.equal(legendValue);
       expect(wrapper.vm.progress).to.equal(progress);
     });
   });
   describe("#noData", () => {
-    const wrapper = mount(Container, {
-      propsData: { noData: true, progress: 50 },
-      stubs: { CountUp: true }
-    });
+    const wrapper = factory({ progress: 50, noData: true });
     it("hides the legend, if true", () => {
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.classes()).to.include("ep-hidden");
@@ -106,36 +80,28 @@ describe("[ EllipseProgressContainer.vue ]", () => {
   });
   describe("#loading", () => {
     it("hides the legend, if true", () => {
-      const wrapper = shallowMount(Container, {
-        propsData: { noData: true, progress: 50 }
-      });
+      const wrapper = factory({ progress: 50, loading: true });
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.classes()).to.include("ep-hidden");
     });
   });
   describe("#fontSize", () => {
     it("sets the font size of the legend correctly", () => {
-      const wrapper = shallowMount(Container, {
-        propsData: { progress: 50, fontSize: "15px" }
-      });
+      const wrapper = factory({ progress: 50, fontSize: "15px" });
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.element.style.fontSize).to.equal("15px");
     });
   });
   describe("#fontColor", () => {
     it("sets the font color of the legend correctly", () => {
-      const wrapper = shallowMount(Container, {
-        propsData: { progress: 50, fontColor: "lime" }
-      });
+      const wrapper = factory({ progress: 50, fontColor: "lime" });
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.element.style.color).to.equal("lime");
     });
   });
   describe("#legendClass", () => {
     it("applies class to circle legend", () => {
-      const wrapper = shallowMount(Container, {
-        propsData: { progress: 50, legendClass: "applied-class" }
-      });
+      const wrapper = factory({ progress: 50, legendClass: "applied-class" });
       const spanWrapper = wrapper.find(".ep-legend--value");
       expect(spanWrapper.classes()).to.include("applied-class");
     });
@@ -143,7 +109,7 @@ describe("[ EllipseProgressContainer.vue ]", () => {
   describe("#slots", () => {
     describe("#legend-value", () => {
       it("renders provided slot content", () => {
-        const wrapper = shallowMount(Container, {
+        const wrapper = shallowMount(VueEllipseProgress, {
           propsData: { progress: 50 },
           slots: {
             "legend-value": '<span id="my-slot">Hello Circle</span>'
@@ -154,7 +120,7 @@ describe("[ EllipseProgressContainer.vue ]", () => {
     });
     describe("#legend-caption", () => {
       it("renders provided slot content", () => {
-        const wrapper = shallowMount(Container, {
+        const wrapper = shallowMount(VueEllipseProgress, {
           propsData: { progress: 50 },
           slots: {
             "legend-caption": '<span id="my-slot">Hello Circle</span>'
