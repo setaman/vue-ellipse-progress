4820a8590375b605f5e534946626e21bcd25b095
Sergej Atamantschuk <sergejatam@gmail.com>
2020-04-22 14:19:52 +0200

    chore: exclude source map from build

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,6 @@
 module.exports = {
   css: {
     extract: false
-  }
-  /* configureWebpack: () => {
-    let config = {};
-    if (process.env.NODE_ENV === "production") {
-      config = {
-        entry: {
-          app: "./src/plugin.js"
-        },
-        optimization: {
-          splitChunks: false
-        }
-      };
-    }
-    config.output = {
-      filename: () => "vue-ellipse-progress.js"
-    };
-    return config;
-  } */
+  },
+  productionSourceMap: false
 };
