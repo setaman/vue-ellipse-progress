3467853cb27db70846dcfeb611d0e6f9e3c39284
Sergej Atamantschuk <sergejatam@gmail.com>
2020-06-06 22:00:29 +0200

    demo: add new beta example

--- demo/src/components/TestFeatures/TestExamples.vue
+++ demo/src/components/TestFeatures/TestExamples.vue
@@ -8,65 +8,21 @@
     />
     <v-container fluid class="fill-height">
       <v-row>
-        <!--<example1 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example2 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example3 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example4 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example5 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example6 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example7 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example8 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example9 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />-->
-
-        <example10 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example11 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
-
-        <example12 :loading="loading" :determinate="determinate" :no-data="noData" :test="true" />
+        <example13 :loading="loading" :determinate="determinate" :no-data="noData" :test="true"/>
       </v-row>
     </v-container>
   </section>
 </template>
 
 <script>
-/*import Example1 from "@/components/Examples/Example1";
-import Example2 from "@/components/Examples/Example2";
-import Example3 from "@/components/Examples/Example3";
-import Example4 from "@/components/Examples/Example4";
-import Example5 from "@/components/Examples/Example5";
-import Example6 from "@/components/Examples/Example6";
-import Example7 from "@/components/Examples/Example7";
-import Example8 from "@/components/Examples/Example8";
-import Example9 from "@/components/Examples/Example9";*/
 import ExamplesControls from "@/components/Examples/ExamplesControls";
-import Example10 from "@/components/Examples/Example10";
-import Example11 from "@/components/Examples/Example11";
-import Example12 from "@/components/Examples/Example12";
+import Example13 from "@/components/Examples/Example13";
 
 export default {
   name: "TestExamples",
   components: {
-    Example12,
-    Example11,
-    Example10,
+    Example13,
     ExamplesControls,
-    /*Example9,
-    Example8,
-    Example7,
-    Example6,
-    Example5,
-    Example4,
-    Example3,
-    Example2,
-    Example1*/
   },
   data: () => ({
     loading: false,
