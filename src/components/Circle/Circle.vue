2c25278757ca9c24ecb5a59e17c4dd3f42f389ad
Sergej Atamantschuk <sergejatam@gmail.com>
2020-09-13 19:14:11 +0200

    fix: set correct determinate loader opacity

--- src/components/Circle/Circle.vue
+++ src/components/Circle/Circle.vue
@@ -25,7 +25,7 @@
     </circle>
     <fade-in-transition>
       <g v-if="showDeterminate">
-        <g style="opacity: 1;">
+        <g style="opacity: 0.45;">
           <circle
             class="ep-circle--determinate animation__loading"
             :r="radius"
