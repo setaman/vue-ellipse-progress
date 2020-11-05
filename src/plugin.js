6c0391d2f5be140ddb5ec38fff25ffe424aa9fb5
Sergej Atamantschuk <sergejatam@gmail.com>
2020-11-05 21:23:03 +0100

    fix!: change plugin initialization

--- src/plugin.js
+++ src/plugin.js
@@ -1,17 +1,3 @@
 import VueEllipseProgress from "./components/VueEllipseProgress.vue";
 
-const install = (Vue, name = "vue-ellipse-progress") => Vue.component(name, VueEllipseProgress);
-
-let globalVue = null;
-if (typeof window !== "undefined") {
-  globalVue = window.Vue;
-} else if (typeof global !== "undefined") {
-  globalVue = global.Vue;
-}
-if (globalVue) {
-  globalVue.use({ install });
-}
-
-export default {
-  install,
-};
+export default (app, name = "vue-ellipse-progress") => app.component(name, VueEllipseProgress);
