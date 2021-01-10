1016e11d0deecc2c80bbef3c9d78837a01562328
Sergej Atamantschuk <sergejatam@gmail.com>
2021-01-10 15:26:54 +0100

    chore: remove tests config fix related to vue-cli bug

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,4 @@ module.exports = {
     extract: false,
   },
   productionSourceMap: false,
-  // see issue https://github.com/vuejs/vue-loader/issues/1734
-  chainWebpack: (config) => {
-    config.module
-      .rule("vue")
-      .use("vue-loader")
-      .loader("vue-loader")
-      .tap((options) => {
-        // eslint-disable-next-line no-param-reassign
-        options.isServerBuild = false;
-        return options;
-      });
-  },
 };
