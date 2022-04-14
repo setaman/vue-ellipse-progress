53b721cd5e12722e928e84d5ce5d6a5dcee77b79
Sergej Atamantschuk <sergejatam@gmail.com>
2022-04-14 16:23:19 +0200

    release: v2.1.1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  "type-enum": [2, "always", ["build", "ci", "docs", "feat", "fix", "refactor", "revert", "test", "release"]],
+  rules: {
+    "type-enum": [2, "always", ["build", "ci", "docs", "feat", "fix", "refactor", "revert", "test", "release"]],
+  },
 };
