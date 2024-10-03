e757c37465f6ce7bc623d648c2ad4152d29726b6
Sergej Atamantschuk <sergejatam@gmail.com>
2024-10-03 17:31:25 +0200

    chore: push legacy docs

--- demo/src/main.js
+++ demo/src/main.js
@@ -4,11 +4,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueEllipseProgress from "vue-ellipse-progress";
-import VueEllipseProgressTest from "vue-ellipse-progress-test";
 import Clipboard from "v-clipboard";
 
 Vue.use(VueEllipseProgress);
-Vue.use(VueEllipseProgressTest, "vue-ellipse-progress-test");
 Vue.use(Clipboard);
 Vue.config.productionTip = false;
 
